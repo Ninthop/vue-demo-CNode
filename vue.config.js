1830b41ba45b1ac0d13cbb9e37681efa0a9120ac
Ninthop <Ninthop@outlook.com>
2019-08-02 16:41:52 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+    },
+  // 打包时不生成.map文件
+  productionSourceMap: false,
+
+  devServer: {
+    open: true
+  }
+}
