97d5b6f826d512d9d95f61641f97e70e17388870
Ninthop <Ninthop@outlook.com>
2019-08-06 09:57:03 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '../' : '/',
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
