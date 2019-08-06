707a0cabe121c7ee317db85aa3002e9bab55f44b
Ninthop <Ninthop@outlook.com>
2019-08-06 09:10:22 +0800

    dist

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 
 export default new Router({
     mode: 'history',
-    base: '/dist/',
+    base: '/vue-demo-cnode/dist',
     routes,
     scrollBehavior (to, from, savedPosition) {
         return { x: 0, y: 0 }
