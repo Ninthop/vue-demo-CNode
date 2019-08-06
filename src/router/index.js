32a9202f0abfa3d4d584a0410d805ebc4c8812e6
Ninthop <Ninthop@outlook.com>
2019-08-06 10:14:44 +0800

    dist

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,6 @@ import routes from './router.js'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
-    base: '/dist',
     routes,
     scrollBehavior (to, from, savedPosition) {
         return { x: 0, y: 0 }
